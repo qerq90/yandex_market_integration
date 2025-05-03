4c5d6d80eae518d9de1dd2c2cfff5c427c628204
qerq90 <alsavidi37@gmail.com>
2025-05-04 01:15:36 +0300

    change place of enrichment to orderService

--- core/src/main/scala/service/order/OrderService.scala
+++ core/src/main/scala/service/order/OrderService.scala
@@ -1,16 +1,22 @@
 package service.order
 
 import dao.order.OrderDao
+import dao.user.UserDao
 import io.circe.Json
 import model.order.Order
+import repository.yandex.YandexRepository
 import zio.{Task, ZLayer}
 
 trait OrderService {
   def saveOrder(order: Json): Task[Unit]
-  def enrichOrder(order: Order): Task[Order]
+  def enrichOrders(orders: List[Order]): Task[Unit]
 }
 
 object OrderService {
-  val live: ZLayer[OrderDao, Nothing, OrderService] =
-    ZLayer.fromFunction(new OrderServiceLive(_))
+  val live: ZLayer[
+    OrderDao with UserDao with YandexRepository,
+    Nothing,
+    OrderService
+  ] =
+    ZLayer.fromFunction(new OrderServiceLive(_, _, _))
 }
--- core/src/main/scala/service/order/OrderServiceLive.scala
+++ core/src/main/scala/service/order/OrderServiceLive.scala
@@ -1,12 +1,20 @@
 package service.order
 
 import dao.order.OrderDao
+import dao.user.UserDao
 import io.circe.Json
 import model.order.Order
+import model.order.Status.Failed
+import model.user.User
 import model.yandex.OrderCreated
+import repository.yandex.YandexRepository
 import zio.{Task, ZIO}
 
-class OrderServiceLive(orderDao: OrderDao) extends OrderService {
+class OrderServiceLive(
+  orderDao: OrderDao,
+  userDao: UserDao,
+  repo: YandexRepository
+) extends OrderService {
 
   override def saveOrder(orderJson: Json): Task[Unit] =
     for {
@@ -17,5 +25,50 @@ class OrderServiceLive(orderDao: OrderDao) extends OrderService {
       _ <- orderDao.saveOrder(order)
     } yield ()
 
-  override def enrichOrder(order: Order): Task[Order] = ???
+  override def enrichOrders(orders: List[Order]): Task[Unit] = {
+    val campaignIds        = orders.map(_.campaignId).distinct
+    val campaignIdToOrders = orders.groupBy(_.campaignId)
+    for {
+      users <- userDao.getUsers(campaignIds)
+
+      _ <- ZIO.foreachParDiscard(campaignIdToOrders) {
+        case (campaignId, ordersToEnrich) =>
+          tryToEnrichOrders(
+            users.find(_.campaignId == campaignId),
+            ordersToEnrich
+          )
+      }
+    } yield ()
+  }
+
+  private def tryToEnrichOrders(
+      userOp: Option[User],
+      orders: List[Order]
+  ): Task[Unit] =
+    userOp match {
+      case None => orderDao.changeStatus(orders, Failed)
+      case Some(user) =>
+        for {
+          offerMappings <- repo.getOfferMapping(user)
+          enrichedOrders = orders.map(order =>
+            order.copy(data =
+              order.data.copy(items =
+                order.data.items.map(item =>
+                  item.copy(name =
+                    Some(
+                      offerMappings
+                        .find(_.offerId == item.offerId)
+                        .map(_.name)
+                        .getOrElse("Имя не найдено")
+                    )
+                  )
+                )
+              )
+            )
+          )
+
+          _ <- orderDao.enrichOrders(enrichedOrders)
+        } yield ()
+    }
+
 }
