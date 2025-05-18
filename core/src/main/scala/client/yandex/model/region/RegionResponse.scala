d8a078b84877d2dc4abbde9a0767a2013a5cb7df
qerq90 <alsavidi37@gmail.com>
2025-05-18 22:25:23 +0300

    remove paging from region

--- core/src/main/scala/client/yandex/model/region/RegionResponse.scala
+++ core/src/main/scala/client/yandex/model/region/RegionResponse.scala
@@ -4,15 +4,9 @@ import io.circe.{Decoder, HCursor}
 import io.circe.generic.semiauto._
 
 // Root level
-case class RegionResponse(regions: List[Region], paging: Paging)
+case class RegionResponse(regions: List[Region])
 object Response {
   implicit val decoder: Decoder[RegionResponse] = deriveDecoder[RegionResponse]
-}
-
-// Paging information
-case class Paging(nextPageToken: String)
-object Paging {
-  implicit val decoder: Decoder[Paging] = deriveDecoder[Paging]
 }
 
 // Region details (recursive)
@@ -24,14 +18,5 @@ case class Region(
   children: List[Option[Region]]
 )
 object Region {
-//  implicit val decoder: Decoder[Region] = (c: HCursor) =>
-//    for {
-//      id        <- c.downField("id").as[Int]
-//      name      <- c.downField("name").as[String]
-//      typeField <- c.downField("type").as[String]
-//      parent    <- c.downField("parent").as[Option[Region]]
-//      children  <- c.downField("children").as[List[Option[Region]]]
-//    } yield Region(id, name, typeField, parent, children)
-
   implicit val decoder: Decoder[Region] = deriveDecoder[Region]
 }
