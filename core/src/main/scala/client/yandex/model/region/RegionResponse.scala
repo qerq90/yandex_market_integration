e8cf7844a48f5e3d4859f34f2529ed65782ae648
qerq90 <alsavidi37@gmail.com>
2025-05-18 22:28:45 +0300

    idiotic api

--- core/src/main/scala/client/yandex/model/region/RegionResponse.scala
+++ core/src/main/scala/client/yandex/model/region/RegionResponse.scala
@@ -9,13 +9,37 @@ object Response {
   implicit val decoder: Decoder[RegionResponse] = deriveDecoder[RegionResponse]
 }
 
+// "regions" : [
+//    {
+//      "id" : 2,
+//      "name" : "Санкт-Петербург",
+//      "type" : "CITY",
+//      "parent" : {
+//        "id" : 10174,
+//        "name" : "Санкт-Петербург и Ленинградская область",
+//        "type" : "REPUBLIC",
+//        "parent" : {
+//          "id" : 17,
+//          "name" : "Северо-Западный федеральный округ",
+//          "type" : "COUNTRY_DISTRICT",
+//          "parent" : {
+//            "id" : 225,
+//            "name" : "Россия",
+//            "type" : "COUNTRY"
+//          }
+//        }
+//      }
+//    }
+//  ]
+//}
+
 // Region details (recursive)
 case class Region(
   id: Int,
   name: String,
   `type`: String,
   parent: Option[Region],
-  children: List[Option[Region]]
+  children: Option[List[Option[Region]]]
 )
 object Region {
   implicit val decoder: Decoder[Region] = deriveDecoder[Region]
