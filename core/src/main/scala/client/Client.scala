e93d8227b98141a2e75c3f28c7eb87fabb92a76f
qerq90 <alsavidi37@gmail.com>
2025-07-13 12:31:56 +0300

    add better timeout

--- core/src/main/scala/client/Client.scala
+++ core/src/main/scala/client/Client.scala
@@ -5,11 +5,14 @@ import org.http4s.ember.client.EmberClientBuilder
 import zio.{Task, ZIO}
 import zio.interop.catz._
 
+import scala.concurrent.duration.DurationInt
+
 object Client {
   def make: Task[HClient[Task]] =
     ZIO.scoped {
       EmberClientBuilder
         .default[Task]
+        .withTimeout(5.minutes)
         .build
         .toScopedZIO
     }
